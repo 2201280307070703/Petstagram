ba655ba898f4f64bf147f7ae9f05e4e981bab350
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-11-05 14:16:32 +0200

    add post model

---
+++ src/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image link is required!']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required!']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!']
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required!']
+    },
+    //comments!
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
